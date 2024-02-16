811ff379849e83dfcf817cfabf6627f0ee39fc6d
Raphael Santos <raphaelsantos.j@gmail.com>
2024-02-16 14:45:58 -0300

    feat: Add item resources to router

--- lib/todo_list_app_web/router.ex
+++ lib/todo_list_app_web/router.ex
@@ -18,6 +18,7 @@ defmodule TodoListAppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    resources "/items", ItemController
   end
 
   # Other scopes may use custom stacks.
